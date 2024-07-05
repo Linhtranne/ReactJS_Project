81e55fdf7182455adfd3b1cf706ce6befa312353
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2024-07-05 16:26:14 +0700

    6166

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Load from "./components/User/loading"
 import Nav from "./components/User/Navbar"
 import Side from "./components/User/Sidebar"
 import Menu from "./components/User/Menu"
-import {BrowserRouter as Routes, Route, Router, Switch} from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Admin from "./components/Admin/Admin"
 
 export default function App() {
@@ -15,12 +15,11 @@ export default function App() {
     <div>
     <Router>
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route path="/login" component={Login} />
         <Route path="/admin" component={Admin} />
+        <Redirect from="/" to="/login" />
       </Switch>
     </Router>
-
     </div>
   )
 }
-
